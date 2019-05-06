d655223b4d9255cb67cc2c5e49324b8fbbd52533
Leila <nb.leila10@gmail.com>
2019-05-05 20:35:49 -0500

    Add home section

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 //when nav button are clicked, they become active
 
-const navBtn = document.querySelector(".navBtn");
 var btns = document.getElementsByClassName("navBtn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
+    const dest = this.getAttribute("dest");
+    location.href = `#${dest}`;
   });
 }
